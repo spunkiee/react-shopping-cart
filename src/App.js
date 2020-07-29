91fe5ffb8920f52a26beab32bc972f77fa8a8019
spunkiee <65862890+spunkiee@users.noreply.github.com>
2020-07-29 18:56:45 +0530

    Added Products Feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from "react";
 import "./index.css";
+import data from "./data.json";
+import Products from "../src/components/Products";
 
-// Feature One
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All Rights Reserved.</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Item</div>
+          </div>
+        </main>
+        <footer>All Rights Reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
