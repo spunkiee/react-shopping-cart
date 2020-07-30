8610fff907ed3dc82600c69c594266b383306895
spunkiee <65862890+spunkiee@users.noreply.github.com>
2020-07-30 17:44:06 +0530

    1. Added local storage feature for Cart Items 2. Added Checkout Component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,32 @@ import React, { Component } from "react";
 import formatCurrency from "../util";
 
 export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      address: "",
+      showCheckout: false,
+    };
+  }
+
+  handleInput = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  createOrder = (e) => {
+    e.preventDefault();
+    const order = {
+      name: this.state.name,
+      email: this.state.email,
+      address: this.state.address,
+      cartItems: this.props.cartItems,
+    };
+
+    this.props.createOrder(order);
+  };
+
   render() {
     const { cartItems } = this.props;
     return (
@@ -38,20 +64,67 @@ export default class Cart extends Component {
             </ul>
           </div>
           {cartItems.length !== 0 && (
-            <div className="cart">
-              <div className="total">
-                <div>
-                  Total:{" "}
-                  {formatCurrency(
-                    cartItems.reduce(
-                      (accumulator, currentValue) =>
-                        accumulator + currentValue.price * currentValue.count,
-                      0
-                    )
-                  )}
+            <div>
+              <div className="cart">
+                <div className="total">
+                  <div>
+                    Total:{" "}
+                    {formatCurrency(
+                      cartItems.reduce(
+                        (accumulator, currentValue) =>
+                          accumulator + currentValue.price * currentValue.count,
+                        0
+                      )
+                    )}
+                  </div>
+                  <button
+                    onClick={() => this.setState({ showCheckout: true })}
+                    className="button primary"
+                  >
+                    Proceed
+                  </button>
                 </div>
-                <button className="button primary">Proceed</button>
               </div>
+              {this.state.showCheckout && (
+                <div className="cart">
+                  <form onSubmit={this.createOrder}>
+                    <ul className="form-container">
+                      <li>
+                        <label>Email</label>
+                        <input
+                          name="email"
+                          type="email"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                        <label>Name</label>
+                        <input
+                          name="name"
+                          type="text"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                        <label>Address</label>
+                        <input
+                          name="address"
+                          type="text"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                        <button type="submit" className="button primary">
+                          Checkout
+                        </button>
+                      </li>
+                    </ul>
+                  </form>
+                </div>
+              )}
             </div>
           )}
         </div>
